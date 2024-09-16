112a1c6729052f85aaf7735ca7d6c9ce157d308f
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-09-16 21:46:04 +0530

    Add Event categories

---
+++ src/app/events/page.js
@@ -0,0 +1,6 @@
+export default function allevents() {
+    return (
+      <div className="text-5xl p-5">payments Page</div>
+    );
+  }
+  
---
+++ src/app/events/technical/page.js
@@ -0,0 +1,6 @@
+export default function technical() {
+    return (
+      <div className="text-5xl p-5">payments Page</div>
+    );
+  }
+  
